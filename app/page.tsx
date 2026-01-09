69d888ec71e522ca89f2c3479f940c324f90cdf6
bilals2008 <wbiz2939@gmail.com>
2026-01-09 17:50:28 +0500

    add next themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// File: app/page.tsx
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
---
+++ components/mode-toggle.tsx
@@ -0,0 +1,34 @@
+// File: components/mode-toggle.tsx
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Moon, SunMoon } from "lucide-react";
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div
+      className="cursor-pointer"
+      onClick={() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      {theme === "light" ? (
+        <Moon className="h-5 w-5 text-black" />
+      ) : (
+        <SunMoon className="h-5 w-5 rotate-180 text-white" color="white" />
+      )}
+    </div>
+  );
+}
