0d2a4aacf11e3f635ad10a7abaccdb7668beedd6
bilals2008 <wbiz2939@gmail.com>
2026-01-11 14:49:52 +0500

    add inter font

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,7 +19,7 @@ export default function HomeLayout({
   return (
     <>
       <Header />
-      <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black">
+      <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black font-inter">
         {children}
       </main>
       <Footer />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // File: app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -11,6 +11,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
