f9f5bcbd39e60c481d15fce5faf07e4bfcc06828
bilals2008 <wbiz2939@gmail.com>
2026-01-11 15:27:18 +0500

    add hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
 // File: app/(root)/page.tsx
+
+import { HeroSection } from "@/components/hero/Hero";
+
 // import EventToast from "@/components/event-toast";
 export default function Home() {
     return (
         <main className="bg-white dark:bg-black/5 overflow-x-hidden">
             <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-6 lg:px-4 gap-4 sm:gap-12">
+                <HeroSection />
             </div>
             {/* <EventToast /> */}
         </main>
