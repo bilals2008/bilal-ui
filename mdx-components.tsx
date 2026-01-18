35077ff3a133538b36d54fb0cc913e6fc3bbd695
bilals2008 <wbiz2939@gmail.com>
2026-01-18 21:20:46 +0500

    feat: add callout component and update docs styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,7 +4,7 @@ import type { MDXComponents } from "mdx/types";
 import { ComponentPreview } from "@/components/bilalUi/component-preview";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import { Step, Steps } from "fumadocs-ui/components/steps";
-import { Callout } from "fumadocs-ui/components/callout";
+import { Callout } from "@/components/ui/callout";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -18,3 +18,4 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...components,
   };
 }
+
