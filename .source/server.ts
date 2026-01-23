a6a1414453bf1a81319275282bf28ab394874023
bilals2008 <wbiz2939@gmail.com>
2026-01-23 21:48:34 +0500

    change style of table of content

--- .source/server.ts
+++ .source/server.ts
@@ -1,4 +1,5 @@
 // @ts-nocheck
+import * as __fd_glob_6 from "../content/docs/getting-started/troubleshooting.mdx?collection=docs"
 import * as __fd_glob_5 from "../content/docs/getting-started/introduction.mdx?collection=docs"
 import * as __fd_glob_4 from "../content/docs/getting-started/installation.mdx?collection=docs"
 import * as __fd_glob_3 from "../content/docs/getting-started/changelog.mdx?collection=docs"
@@ -13,4 +14,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "getting-started/meta.json": __fd_glob_1, }, {"components/button.mdx": __fd_glob_2, "getting-started/changelog.mdx": __fd_glob_3, "getting-started/installation.mdx": __fd_glob_4, "getting-started/introduction.mdx": __fd_glob_5, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "getting-started/meta.json": __fd_glob_1, }, {"components/button.mdx": __fd_glob_2, "getting-started/changelog.mdx": __fd_glob_3, "getting-started/installation.mdx": __fd_glob_4, "getting-started/introduction.mdx": __fd_glob_5, "getting-started/troubleshooting.mdx": __fd_glob_6, });
--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -10,6 +10,8 @@ import { notFound } from "next/navigation";
 import { getMDXComponents } from "@/mdx-components";
 import type { Metadata } from "next";
 import { createRelativeLink } from "fumadocs-ui/mdx";
+import * as Icons from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
   const params = await props.params;
@@ -19,7 +21,87 @@ export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage 
+      toc={page.data.toc} 
+      full={page.data.full}
+      tableOfContent={{
+        style: 'clerk',
+      
+        footer: (
+          <div className="flex flex-col gap-4 text-sm text-muted-foreground mt-4 pt-4 border-t border-dashed border-border/70">
+            <div className="flex flex-col gap-2">
+              <span className="font-medium text-foreground flex items-center gap-2">
+                <Icons.LifeBuoy className="w-4 h-4 text-primary" />
+                Common Issues
+              </span>
+              <a 
+                href="https://github.com/bilals2008/bilal-ui/issues?q=is%3Aissue+Component+not+found" 
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center gap-2 hover:text-foreground transition-colors group"
+              >
+                <div className="flex items-center justify-center w-5 h-5 rounded-md bg-red-500/10 text-red-500 group-hover:bg-red-500 group-hover:text-white transition-all">
+                  <Icons.CircleAlert className="w-3 h-3" />
+                </div>
+                <span>Component not found?</span>
+              </a>
+              <a 
+                href="https://github.com/bilals2008/bilal-ui/issues?q=is%3Aissue+Styles+not+applying" 
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center gap-2 hover:text-foreground transition-colors group"
+              >
+                <div className="flex items-center justify-center w-5 h-5 rounded-md bg-blue-500/10 text-blue-500 group-hover:bg-blue-500 group-hover:text-white transition-all">
+                  <Icons.Palette className="w-3 h-3" />
+                </div>
+                <span>Styles not applying?</span>
+              </a>
+               <a 
+                href="https://github.com/bilals2008/bilal-ui/issues?q=is%3Aissue+TypeScript+errors" 
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center gap-2 hover:text-foreground transition-colors group"
+              >
+                <div className="flex items-center justify-center w-5 h-5 rounded-md bg-yellow-500/10 text-yellow-500 group-hover:bg-yellow-500 group-hover:text-white transition-all">
+                  <Icons.FileJson className="w-3 h-3" />
+                </div>
+                <span>TypeScript errors?</span>
+              </a>
+            </div>
+            
+            <div className="flex flex-col gap-2">
+              <span className="font-medium text-foreground flex items-center gap-2">
+                <Icons.MessageCircleQuestion className="w-4 h-4 text-primary" />
+                Need Help?
+              </span>
+              <a 
+                href={`https://github.com/bilals2008/bilal-ui/blob/main/content/docs/${(params.slug || []).join("/")}.mdx`}
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center gap-2 hover:text-foreground transition-colors group mb-2"
+              >
+                 <div className="flex items-center justify-center w-5 h-5 rounded-md bg-stone-500/10 text-stone-500 group-hover:bg-stone-500 group-hover:text-white transition-all">
+                  <Icons.Github className="w-3 h-3" />
+                </div>
+                <span className="group-hover:underline">Edit on GitHub</span>
+              </a>
+
+              <a 
+                href="https://github.com/bilals2008/bilal-ui/issues/new" 
+                target="_blank" 
+                rel="noreferrer"
+                className="flex items-center gap-2 hover:text-foreground transition-colors group"
+              >
+                 <div className="flex items-center justify-center w-5 h-5 rounded-md bg-green-500/10 text-green-500 group-hover:bg-green-500 group-hover:text-white transition-all">
+                  <Icons.Bug className="w-3 h-3" />
+                </div>
+                <span className="group-hover:underline">Open an Issue</span>
+              </a>
+            </div>
+          </div>
+        )
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
