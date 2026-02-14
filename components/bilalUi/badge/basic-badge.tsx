7fae8d2e11f78aede1898c80024ad3622e5944fa
bilals2008 <wbiz2939@gmail.com>
2026-02-14 10:55:43 +0500

    add basic badge

---
+++ components/bilalUi/badge/basic-badge.tsx
@@ -0,0 +1,10 @@
+// File: components/bilalUi/badge/basic-badge.tsx
+import { Badge } from "@/components/ui/badge";
+
+export function BasicBadge() {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <Badge variant="primary">Default</Badge>
+    </div>
+  );
+}
