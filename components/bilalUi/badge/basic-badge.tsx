96ec66ad9c38456c44e43a50612f7b58c9702780
bilals2008 <wbiz2939@gmail.com>
2026-02-14 11:44:27 +0500

    refactor basic badge

--- components/bilalUi/badge/basic-badge.tsx
+++ components/bilalUi/badge/basic-badge.tsx
@@ -3,8 +3,17 @@ import { Badge } from "@/components/ui/badge";
 
 export function BasicBadge() {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="grid grid-cols-4 gap-4">
       <Badge variant="primary">Default</Badge>
+      <Badge variant="primary" appearance="light">
+        Light
+      </Badge>
+      <Badge variant="primary" appearance="outline">
+        Outline
+      </Badge>
+      <Badge variant="primary" appearance="ghost">
+        Ghost
+      </Badge>
     </div>
   );
 }
