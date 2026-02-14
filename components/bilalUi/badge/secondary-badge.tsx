57d2e80f7faa8cb7ea8ffd297e98163e30f05060
bilals2008 <wbiz2939@gmail.com>
2026-02-14 11:45:16 +0500

    refactor secondary badge

--- components/bilalUi/badge/secondary-badge.tsx
+++ components/bilalUi/badge/secondary-badge.tsx
@@ -3,8 +3,17 @@ import { Badge } from "@/components/ui/badge";
 
 export function SecondaryBadge() {
   return (
-    <div className="flex w-full items-center justify-center gap-2">
-      <Badge variant="secondary">Secondary</Badge>
+    <div className="flex w-full flex-wrap items-center justify-center gap-2">
+      <Badge variant="secondary">Default</Badge>
+      <Badge variant="secondary" appearance="light">
+        Light
+      </Badge>
+      <Badge variant="secondary" appearance="outline">
+        Outline
+      </Badge>
+      <Badge variant="secondary" appearance="ghost">
+        Ghost
+      </Badge>
     </div>
   );
 }
