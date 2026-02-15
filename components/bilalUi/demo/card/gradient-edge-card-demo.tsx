fb0c0803f30c844324140331e4da227efea4cf50
bilals2008 <wbiz2939@gmail.com>
2026-02-15 15:41:59 +0500

    add gradiant edge card

---
+++ components/bilalUi/demo/card/gradient-edge-card-demo.tsx
@@ -0,0 +1,9 @@
+import GradientEdgeCard from "@/components/bilalUi/card/gradient-edge-card";
+
+export default function GradientEdgeCardDemo() {
+  return (
+    <div className="flex w-full items-center justify-center p-4">
+      <GradientEdgeCard />
+    </div>
+  );
+}
