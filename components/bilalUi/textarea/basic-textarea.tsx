66a7448a7b8b087fa08370fdfe121bed23a7acff
bilals2008 <wbiz2939@gmail.com>
2026-02-19 15:07:28 +0500

    (textarea): add basic textarea

---
+++ components/bilalUi/textarea/basic-textarea.tsx
@@ -0,0 +1,11 @@
+// File: components/bilalUi/textarea/basic-textarea.tsx
+import { Textarea } from "@/components/ui/textarea";
+
+export function BasicTextarea() {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-4">
+      <Textarea placeholder="Type your message here." />
+      <Textarea disabled placeholder="Disabled textarea." />
+    </div>
+  );
+}
