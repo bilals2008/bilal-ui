37e28f8b48e9b994d1b50fd97612d2fea1ef03f9
bilals2008 <wbiz2939@gmail.com>
2026-02-20 23:17:16 +0500

    (dialog): add edit profile variant

---
+++ components/bilalUi/demo/dialog/edit-profile-dialog-demo.tsx
@@ -0,0 +1,6 @@
+// File: components/bilalUi/demo/dialog/edit-profile-dialog-demo.tsx
+import { EditProfileDialog } from "@/components/bilalUi/dialog/edit-profile-dialog";
+
+export default function EditProfileDialogDemo() {
+  return <EditProfileDialog />;
+}
