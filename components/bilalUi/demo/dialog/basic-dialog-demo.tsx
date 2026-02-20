6da2da16e46521b2a03f4903269cfcf68837f0c4
bilals2008 <wbiz2939@gmail.com>
2026-02-20 23:16:44 +0500

    (dialog): add basic dialog

---
+++ components/bilalUi/demo/dialog/basic-dialog-demo.tsx
@@ -0,0 +1,6 @@
+// File: components/bilalUi/demo/dialog/basic-dialog-demo.tsx
+import { BasicDialog } from "@/components/bilalUi/dialog/basic-dialog";
+
+export default function BasicDialogDemo() {
+  return <BasicDialog />;
+}
