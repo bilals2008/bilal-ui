9e738004e9f24b7ade22a69ef8c44c9f121f713a
bilals2008 <wbiz2939@gmail.com>
2026-02-23 18:06:00 +0500

    chore(skeleton): keep only basic variant

--- components/bilalUi/demo/skeleton/card-skeleton-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { CardSkeleton } from "@/components/bilalUi/skeleton/card-skeleton";
-
-export default function CardSkeletonDemo() {
-  return (
-    <div className="flex items-center justify-center p-8">
-      <CardSkeleton />
-    </div>
-  );
-}
