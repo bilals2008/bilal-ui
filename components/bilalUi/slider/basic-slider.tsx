cbc4915e38bb5414ecd54134e49a2065ce7904fb
bilals2008 <wbiz2939@gmail.com>
2026-02-23 22:37:17 +0500

    feat(slider): add basic slider component

---
+++ components/bilalUi/slider/basic-slider.tsx
@@ -0,0 +1,9 @@
+import { Slider } from "@/components/ui/slider";
+
+export function BasicSlider() {
+  return (
+    <div className="w-full max-w-sm">
+      <Slider defaultValue={[50]} max={100} step={1} />
+    </div>
+  );
+}
