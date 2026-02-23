436df1c71810ce670eed52fa30b4016dc50085f0
bilals2008 <wbiz2939@gmail.com>
2026-02-23 18:06:01 +0500

    chore(dropdown-menu): keep only basic variant

--- components/bilalUi/demo/dropdown-menu/preferences-dropdown-menu-demo.tsx
+++
@@ -1,5 +0,0 @@
-import { PreferencesDropdownMenu } from "@/components/bilalUi/dropdown-menu/preferences-dropdown-menu";
-
-export default function PreferencesDropdownMenuDemo() {
-  return <PreferencesDropdownMenu />;
-}
