5d6d0d58daf403559cd315926716f8a66232aed4
bilals2008 <wbiz2939@gmail.com>
2026-02-23 18:06:01 +0500

    chore(alert-dialog): keep only primary variant

--- components/bilalUi/demo/alert-dialog/type-to-confirm-alert-dialog-demo.tsx
+++
@@ -1,6 +0,0 @@
-// File: components/bilalUi/demo/alert-dialog/type-to-confirm-alert-dialog-demo.tsx
-import { TypeToConfirmAlertDialog } from "@/components/bilalUi/alert-dialog/type-to-confirm-alert-dialog";
-
-export default function TypeToConfirmAlertDialogDemo() {
-  return <TypeToConfirmAlertDialog />;
-}
