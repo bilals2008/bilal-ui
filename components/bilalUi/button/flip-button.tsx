b55c3aa56fe56d02f91972464258a60fe42edf9d
bilals2008 <wbiz2939@gmail.com>
2026-03-13 23:21:14 +0500

    feat(button): add flip-button

---
+++ components/bilalUi/button/flip-button.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import * as React from "react";
+import { motion } from "motion/react";
+
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export const FlipButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }, ref) => (
+    <motion.div whileHover={{ rotateX: 10 }} whileTap={{ scale: 0.95 }} perspective={1000}>
+      <Button ref={ref} className={cn("transition-transform", className)} {...props}>
+        {children}
+      </Button>
+    </motion.div>
+  ),
+);
+FlipButton.displayName = "FlipButton";
