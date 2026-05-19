2245f4219a4d8eb994225e3290672d4025939f8a
bilals2008 <wbiz2939@gmail.com>
2026-05-20 01:13:29 +0500

    feat: add reusable Pro components (ProBadge, ProCard, ProFeatureCard, ProComingSoon)

---
+++ components/bilalUi/pro/index.ts
@@ -0,0 +1,5 @@
+// File: components/bilalUi/pro/index.ts
+export { ProBadge } from "./pro-badge";
+export { ProCard } from "./pro-card";
+export { ProFeatureCard } from "./pro-feature-card";
+export { ProComingSoon } from "./pro-coming-soon";
